17b7adc28eed7350a194633b94694caa9cfde7b2
Fred Uggla <fred.uggla@gmail.com>
2016-11-22 17:16:17 +0100

    Corrected css paths

--- render.sh
+++ render.sh
@@ -1,1 +1,1 @@
-pandoc --toc -c https://fonts.googleapis.com/css?family=Open+Sans -c normalize.css -c style.css -s -o index.html index.md
+pandoc --toc -c https://fonts.googleapis.com/css?family=Open+Sans -c css/normalize.css -c css/style.css -s -o index.html index.md
